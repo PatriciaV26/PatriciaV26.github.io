6fa89c3f0740f3fe4baec58ad14fc3525ee207fa
Patricia Veres <veres_patricia@yahoo.com>
2019-03-25 11:30:42 +0200

    show skills with for

--- intro.js
+++ intro.js
@@ -51,3 +51,13 @@ function hideAllPages(){
 }
 
 initMenu();
+
+function showSkills() {
+    var skills = ['html','css', 'js'];
+    console.warn('showSkills', skills);
+    for(var i=0; i< skills.lenght; i++) {
+        console.info(i + 1 + ". " + skills[i]);
+    }
+}
+
+showSkills();
